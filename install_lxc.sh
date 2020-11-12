a36bf88ced45d8def35801ee05ddbace02ac61f6
tomatolike <695268122@qq.com>
2020-11-12 09:42:19 -0500

    fix lxc bug

--- install_lxc.sh
+++ install_lxc.sh
@@ -8,9 +8,9 @@ echo 'lxc.cgroup.memory.limit_in_bytes = 4G' >> memcached.conf
 echo 'test' > memcached.log
 sudo lxc-create -n memcached -f memcached.conf -t ubuntu
 sudo lxc-start -d -n memcached --console-log memcached.log
-sudo lxc-attach -n memcached -- /bin/bash -c "sudo apt-get update && sudo apt-get install -y memcached"
 echo "lxc started, wait for several seconds"
 sleep 5
+sudo lxc-attach -n memcached -- /bin/bash -c "sudo apt-get update && sudo apt-get install -y memcached"
 lxcip=$(sudo lxc-attach -n memcached -- /bin/bash -c "ifconfig | egrep '10.0.3' | cut -d : -f 2 | cut -d ' ' -f 1 | head -n 1")
 echo ${lxcip} > ~/lxcip.txt
 sshpass -p ubuntu ssh ubuntu@${lxcip} -f 'memcached -d -m 65535'
