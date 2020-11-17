6955fbebe70183a6de3ae3e69725d1700ce8ef2c
tomatolike <695268122@qq.com>
2020-11-16 19:44:31 -0500

    fix bug

--- establish.sh
+++ establish.sh
@@ -1,4 +1,4 @@
 bash Infiniswap/install_essential.sh
-bash Infiniswap/install_infinibs.sh 192.168.0.9
+bash Infiniswap/install_infinibd.sh 192.168.0.9
 bash Infiniswap/install_memaslap.sh
 bash Infiniswap/install_lxc.sh
